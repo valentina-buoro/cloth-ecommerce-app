df06e70a6700d287153cfd363ba8dd3c6804cfc6
valentina buoro <valentinabuoro@valentinas-MacBook-Air.local>
2023-06-25 11:59:22 +0100

    fix

--- src/components/Products/Card.js
+++ src/components/Products/Card.js
@@ -15,7 +15,7 @@ const Card = (props) => {
   return (
     <div class="flex flex-col justify-between bg-white p-3 rounded-md shadow-md ">
       <div className=" h-[150px] md:h-[300px] self-center">
-        <img className="w-auto h-full object-none " src={props.item.image} alt="pic" />
+        <img className="w-auto h-full object-cover " src={props.item.image} alt="pic" />
       </div>
      <div className="py-2 md:py-4">
      <div className="flex flex-col md:flex-row md:justify-between">
