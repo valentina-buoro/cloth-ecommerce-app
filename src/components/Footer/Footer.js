ecc19719ed3e0dbdc6938ea742f4fff049550725
valentina buoro <valentinabuoro4@gmail.com>
2023-08-11 23:17:49 +0100

    footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,13 +1,37 @@
 import React from 'react'
+import { FaTwitter, FaLinkedin,FaGithub } from 'react-icons/fa'
 
 const Footer = () => {
   return (
-    <div>
+    <div className='bg-gray-900 text-white flex flex-col md:flex-row items-center p-6 md:p-12 justify-around'> 
         <div>
             Address
         </div>
         <div>
             social media links
+            <div className=' p-3 flex flex-row gap-4' style={{'marginTop':'2rem'}}>
+                <div className='flex flex-col items-center justify-center gap-2'> 
+                <div className='md:p-4'>
+                <a href='https://github.com/queen-x-vee' className=''><FaGithub size={30} style={{color:'#fff'}} /></a>
+                </div>
+                <span>Github</span>
+                </div>
+               <div className='flex flex-col items-center justify-center gap-2' >
+               <div className='md:p-4'>
+               <a href='https://twitter.com/omose_B'><FaTwitter size={30} style={{color:'#fff'}} /></a>
+               </div>
+               <div>
+               <span>X </span>
+               <span className='text-[0.675rem] mt-2'>(formerly known as Twitter)</span>
+               </div>
+               </div>
+                <div className='flex flex-col items-center justify-center gap-2'>
+                <div className='md:p-4'>
+                <a href='https://linkedin.com/in/valentina-buoro'><FaLinkedin size={30} style={{color:'#fff'}} /></a>
+                </div>
+                <span>Linkedin</span>
+                </div>
+                </div>
         </div>
     </div>
   )
