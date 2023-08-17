17db878c6b072db29d6d823b0d252ad09349d56c
valentina buoro <valentinabuoro4@gmail.com>
2023-08-17 20:33:08 +0100

    login 2

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,16 +1,26 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const Login = () => {
+    const [formData, setFormData] = useState({
+        email:'', 
+        password:''
+    })
+    const handleChange = (event) => {
+        const { name, value } = event.target;
+        setFormData({ ...formData, [name]: value });
+        console.log(formData)
+      };
+
   return (
     <div>
         <header>
             <span>welcome to <i>Zoren</i></span>
         </header>
         <form>
-            <input type='text' id='email' required/>
+            <input onChange={handleChange} name='email' type='text' id='email' required/>
             <label htmlFor='email'>Email</label>
 
-            <input type='text' id='password' required/>
+            <input onChange={handleChange} name='password' type='text' id='password' required/>
             <label htmlFor='password'>Password</label>
         </form>
     </div>
