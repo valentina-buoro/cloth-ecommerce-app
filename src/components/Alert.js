d13995a4b6fea251e7e8b87d6e9ee11f078d0770
valentina buoro <valentinabuoro4@gmail.com>
2023-08-24 16:23:02 +0100

    alert fix

--- src/components/Alert.js
+++ src/components/Alert.js
@@ -1,18 +1,13 @@
 import Alert from 'react-bootstrap/Alert';
-import { Button } from 'react-bootstrap';
 import {Modal} from 'react-bootstrap';
 
 function InfoAlert(props) {
   return (
     <>
       
-        <Modal show={props.show} onHide={props.closeAlert}>
-        <Alert  key={props.item} variant={props.variant} >
+        <Modal show={props.show} >
+        <Alert  key={props.item} variant={props.variant} onClose={props.closeAlert} dismissible >
           Product successfully added
-
-          <Button onClick={props.closeAlert}  variant="outline-success">
-            Close me
-          </Button>
         </Alert>
         </Modal>
    
